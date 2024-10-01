b56d4db2ab4b4f53500592a7e29177ef52ce7077
Alan <alan.teixido.7e7@itb.cat>
2024-10-01 19:55:16 +0200

    Refactor HTML and JavaScript code for Activitat 2

--- activitat2.js
+++ activitat2.js
@@ -1,5 +1,4 @@
 // Exercici 1
-
 const estudiant = {
     nom: 'Joan',
     edat: 20,
@@ -10,25 +9,62 @@ const estudiant = {
     }
 };
 
+
 function desestrocturacio() {
     const { nom, edat, notes } = estudiant;
-    const result = `${nom} té ${edat} anys i ha tret un ${notes.matematiques} a matemàtiques`;
+    document.getElementById("solucion1").textContent = `${nom} té ${edat} anys i ha tret un ${notes.matematiques} a matemàtiques`;
 
-    const resultElement = document.getElementById("solucion1");
 
-    resultElement.innerHTML = `<p>${result}</p>`;
 
 }
-
 
 // Exercici 2
 const coordenades = [12, 5, 8];
 
 function desestrocturacioarrays() {
     const [x, y, z] = coordenades;
-    const result = `Les coordenades són: x = ${x}, y = ${y}, z = ${z}`;
+    document.getElementById("solucion2").textContent = `Les coordenades són: x = ${x}, y = ${y}, z = ${z}`;
 
-    const resultElement = document.getElementById("solucion2");
+  
+}
 
-    resultElement.innerHTML = `<p>${result}</p>`;   
+//Exercici 3
+const preus = [100, 200, 300, 400];
+
+function metodemap(){
+    const preusIVA = preus.map(preu => preu * 1.21);
+    document.getElementById("solucion3").textContent = `Els preus amb IVA són: ${preusIVA}`;
+
+
+}
+
+//Exercici 4
+const edats = [16, 21, 18, 24, 12, 30, 40];
+
+
+function metodofilter(){
+    const majorsEdat = edats.filter(edat => edat >= 18 && edat <= 30);
+    document.getElementById("solucion4").textContent = `Les persones majors d'edat són: ${majorsEdat}`;
+
+}
+
+//Exercici 5
+const notes = [7, 9, 5, 10, 8];
+
+function metodoreduce(){
+    const notaMitjana = notes.reduce((acc, nota) => acc + nota) / notes.length;
+    document.getElementById("solucion5").textContent = `La nota mitjana és: ${notaMitjana}`;
+
+}
+
+//Exercici 6
+const producte = {
+    nom: "Portàtil",
+    preu: 950,
+    quantitat: 3
+};
+
+function crearobjecteproducte(){
+    document.getElementById("solucion6").textContent = `El producte és: ${producte.nom}, el preu és: ${producte.preu} i la quantitat és: ${producte.quantitat}`;
+    
 }
